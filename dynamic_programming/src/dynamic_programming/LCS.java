be17f60a7a615a7e058595a42563a34cd4ecd191
rksharma0 <rkcusat040@gmail.com>
2022-02-28 22:46:27 +0530

    update

--- dynamic_programming/src/dynamic_programming/LCS.java
+++ dynamic_programming/src/dynamic_programming/LCS.java
@@ -1,50 +1,39 @@
 package dynamic_programming;
 import java.util.*;
-class compa implements Comparator<student>{
 
 	
-	public int compare(student o1, student o2) {
-		
-		return o1.roll-o2.roll;
-	}
-	
-	
-}
-class student{
-	int roll;
-	String name;
-	String add;
-	student(int roll,String name,String add){
-		
-		this.roll=roll;
-		this.name=name;
-		this.add=add;
-	}
-	
-}
+
 public class LCS {
 
 	public static void main(String[] args) {
 		
-		student arr[]=new student[5];
-	//BufferedReader br=new BufferedReader(new InputStreamReader());
-		Scanner scn=new Scanner(System.in);
-//		for(int i=0;i<5;i++) {
-//			
-//			int roll=scn.nextInt();
-//			String name=scn.next();
-//			String add=scn.next();
-//			arr[i]=new student(roll,name,add);
-//		}
-		arr[0]=new student(3,"raman","patna");
-		arr[1]=new student(4,"raj","arwal");
-		arr[2]=new student(1,"santosh","gaya");
-		arr[3]=new student(2,"nischay","punpun");
-		arr[4]=new student(5,"basant","khagriya");
-		//Arrays.sort(arr,new compa());
-for(int i=0;i<5;i++) {
+	    String s1 = "AGGTAB";
+	    String s2 = "GXTXAYB";
+        System.out.println(LCS(s1,s2));
+
+   }
+
+	private static int LCS(String s1, String s2) {
+		int n=s1.length();
+		int m=s2.length();
+		int dp[][]=new int[n+1][m+1];
+		for(int i=0;i<=n;i++) {
 			
-		  System.out.println(arr[i].roll+" "+arr[i].name+" "+arr[i].add);
+			for(int j=0;j<=m;j++) {
+				
+				
+				if(i==0||j==0) {
+					
+					dp[i][j]=0;
+				}else if(s1.charAt(i-1)==s2.charAt(j-1)) {
+					
+					dp[i][j]=dp[i-1][j-1]+1;
+				}else {
+					
+					dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+				}
+			}
 		}
+		return dp[n][m];
 	}
 }
