598e73fcc2b4905c0c6ebd99590bc1971a5a3a71
rksharma0 <rkcusat040@gmail.com>
2022-02-28 21:08:56 +0530

    dp program

---
+++ dynamic_programming/src/dynamic_programming/Coin_change.java
@@ -0,0 +1,19 @@
+package dynamic_programming;
+import java.util.*;
+public class Coin_change {
+
+	public static void main(String[] args) {
+		
+		int arr[]= {1,2,3};
+		int n=4;
+		ArrayList<Integer>res=new ArrayList<>();
+		Solve(arr,n,res);
+		System.out.println(res);
+	}
+
+	private static void Solve(int[] arr, int n, ArrayList<Integer> res) {
+		
+		
+	}
+	
+}
